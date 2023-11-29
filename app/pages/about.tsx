53f8d946b1af1651751a19739fca6f80ab4978ea
Harsh Jha <harshjha19101997@gmail.com>
2023-11-29 12:24:53 +0530

    Resolved Error of babel compiler and started using next Font and nextConfiguration done

---
+++ app/pages/about.tsx
@@ -0,0 +1,13 @@
+// pages/about.tsx
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page content.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    // Your custom webpack configurations can go here
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
